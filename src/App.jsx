57fe6f46915661f64c4d883032a6fa201b5a3fd2
Luke Hewitt <lukehewitt@smashit.games>
2024-09-24 11:25:10 +1000

    changed import of images

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ import ReactPlayer from 'react-player';
 import Modal from 'react-modal';
 import Confetti from 'react-confetti';
 import './App.css';
+import img1 from './images/1.jpg';
+import img2 from './images/2.jpg';
+import img3 from './images/3.jpg';
+import img4 from './images/4.jpg';
+import img5 from './images/5.jpg';
+import img6 from './images/6.jpg';
 
 Modal.setAppElement('#root'); // For accessibility
 
@@ -35,14 +41,7 @@ function App() {
     // State to manage selected video URL
     const [selectedVideoUrl, setSelectedVideoUrl] = useState('');
 
-    const images = [
-        '/images/1.jpg',
-        '/images/2.jpg',
-        '/images/3.jpg',
-        '/images/4.jpg',
-        '/images/5.jpg',
-        '/images/6.jpg',
-    ];
+    const images = [img1, img2, img3, img4, img5, img6];
 
     useEffect(() => {
         // Update window dimensions on resize for confetti
